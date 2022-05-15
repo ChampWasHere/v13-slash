7d14275e8a2c06dfd9bf89106bc8ac64cb8be39b
Ćhàmp? <101576910+ChampWasHere@users.noreply.github.com>
2022-05-15 18:46:36 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# v13-slash
-Slash Komutlu v13 kayıt
+# Pasador Code | V13 Kayıt Botu (Slash Commands)
+TEK YAPMANIZ GEREKEN <code>config.js</code> DOSYASINI DOLDURMAK / BU ARADA V13 + SADECE SLASH KOMUTLARI BULUNMAKTA
+
+Pasador Code Youtube | https://www.youtube.com/channel/UCeRUwW2JQyI3WuERVYkfF8g
---
+++ config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    token: "OTc1MzYyOTg5OTgzMTQ2MDM2.G0BJZv.QuivdgZbsoonhYePpQ9MrH3JcrYld7uhXkxV4I",
+    prefix: ".",
+    activityName: "Kayıt botu Deneme!",
+    twitchChannelName: "dearbotxd",
+
+
+    guildID: "971665095014899712",
+
+
+    RegisterHammer: ["975361913154330635"],
+
+    manRoles: ["975361859718889482", "975361859718889482"],
+    womanRoles: ["975361882829496330", "975361882829496330"],
+    unregRole: "975361958138245151",
+    susRole: "975361982402273370",
+    botRole: "975362324628131860",
+    boosterRole: "975362041667809331",
+
+    regChannel: "975362543491096577",
+    logChannel: "971665095014899715",
+    rulesChannel: "975362557298753556",
+
+    serverTag: "☦",
+    unTag: "",
+
+    embedFooter: "Shawty Kayıt Bot!"
+}
